7bb2b1c1dcfcc14e45110b8ee4d3f6cf5a4d4608
Βιτάλι Τκατσένκο <tkachebco0110@gmail.com>
2023-05-17 20:51:44 +0300

    Добавил форму редактирование поставщиков

--- Aptechka/Forms/ProducersForm.xaml.cs
+++ Aptechka/Forms/ProducersForm.xaml.cs
@@ -19,28 +19,117 @@ namespace AptechkaWPF
     {
 
         private AptechkaContext dbcontext;
+        private ContextMenu contextMenu;
 
-        /// <summary>
-        /// Конструктор формы списка заявок или списка производителей
-        /// </summary>
         public ProducersForm(AptechkaContext dbContext)
         {
             InitializeComponent();
 
             dbcontext = dbContext;
+
+            // Создаём контекстное меню для элемента DataGrid
+            //{
+            contextMenu = new ContextMenu();
+
+            MenuItem mi = new MenuItem();
+            mi.Header = "Редактировать";
+            mi.Tag = 1;
+            mi.Click += Menu_EditItem;
+            contextMenu.Items.Add(mi);
+
+            mi = new MenuItem();
+            mi.Header = "Добавить";
+            mi.Tag = 2;
+            mi.Click += Menu_AddItem;
+            contextMenu.Items.Add(mi);
+
+            mi = new MenuItem();
+            mi.Header = "Удалить";
+            mi.Tag = 3;
+            mi.Click += Menu_DeleteItem;
+            contextMenu.Items.Add(mi);
+            //}
+
+            fDataGrid.ContextMenu = contextMenu;
         }
 
         /// <summary>
-        /// Обработчик двойного нажатия левой кнопки мыши на списке поставщиков
+        /// Обработчик контекстного пункта меню "Редактировать"
+        /// открывает форму редактирования текущей записи
+        /// <return>Не возвращает ничего</return>
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+        private void Menu_EditItem(object sender, RoutedEventArgs e)
+        {
+            OpenEditForm();
+        }
+
+        /// <summary>
+        /// Обработчик контекстного пункта меню "Добавить"
+        /// открывает форму редактирования новой записи
+        /// <return>Не возвращает ничего</return>
+        /// </summary>
+        private void Menu_AddItem(object sender, RoutedEventArgs e)
+        {
+            OpenEditForm(1);
+        }
+
+        /// <summary>
+        /// Обработчик контекстного пункта меню "Удалить"
+        /// удаляет текущую строку из базы данных
+        /// <return>Не возвращает ничего</return>
+        /// </summary>
+        private void Menu_DeleteItem(object sender, RoutedEventArgs e)
+        {
+            Producer drg = (Producer)fDataGrid.SelectedItem;
+
+            if (drg != null)
+            {
+
+                MessageBoxResult rez = MessageBox.Show("Вы действительно хотите удалить запись " + drg.Name + "?", "Винимание!", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+
+                if (rez != MessageBoxResult.Yes) { return; }
+
+                dbcontext.Producers.Remove(drg);
+
+                try
+                {
+                    dbcontext.SaveChanges();
+                }
+                catch (DbUpdateException)
+                {
+                    System.Windows.MessageBox.Show("Произошла ошибка при удалении строки!\n" + e);
+                }
+
+            }
+
+            ShowProducers();
+        }
+
+        /// <summary>
+        /// Обработчик двойного нажатия в элементе DataGrid. При двойном щелчке
+        /// открывается форма редактирования выбранной строки.
+        /// <return>Не возвращает ничего</return>
+        /// </summary>
         private void fDataGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            var prd = fDataGrid.SelectedItem;
-            if (prd != null)
+            OpenEditForm();
+        }
+
+        /// <summary>
+        /// Процедура открывает форму редактирования поставщика
+        /// </summary>
+        /// <param name="addnew">Если 1 - создаётся новый, если 0 - открывается выбранный</param>
+        private void OpenEditForm(int addnew = 0)
+        {
+            Producer req = (Producer)fDataGrid.SelectedItem;
+
+            if ((addnew == 1) || (req == null))
             {
-                System.Windows.MessageBox.Show("Здесь редактируем выбранную строку" + ", " + ((Producer)prd).Name);
+                MainWindow.GetNavFrame().Navigate(new ProducerEditForm(dbcontext));
+            }
+            else
+            {
+                MainWindow.GetNavFrame().Navigate(new ProducerEditForm(dbcontext, req));
             }
         }
 
@@ -75,12 +164,22 @@ namespace AptechkaWPF
             ShowProducers();
         }
 
+
+        /// <summary>
+        /// Обработчик нажатия кнопки "Добавить"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btAdd_Click(object sender, RoutedEventArgs e)
+        {
+            OpenEditForm(1);
+        }
         /// <summary>
         /// Обработчик строки поиска. При изменении данные перезапрашиваются
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void tbSearch_TextChanged(object sender, TextChangedEventArgs e)
+        private void tbSearch_TextChanged_1(object sender, TextChangedEventArgs e)
         {
             ShowProducers();
         }
